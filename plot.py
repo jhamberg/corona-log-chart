e4f1bfbd08a15a9f63cd257c2310e703e6bb690e
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2020-03-22 00:15:00 +0200

    Initial commit

---
+++ plot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.7
+
+import time
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, NullFormatter
+
+countries = [
+    'Finland',
+    'France',
+    'Germany',
+    'Iran',
+    'Italy',
+    'Korea, South',
+    'Spain',
+    'Sweden',
+    'US'
+    'United Kingdom',
+]
+
+url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+dskip = 30 # Days to skip
+fig = pd.read_csv(url) \
+    .drop(columns=["Lat", "Long"]) \
+    .query('`Country/Region` == @countries') \
+    .groupby("Country/Region", as_index=False) \
+    .sum() \
+    .set_index("Country/Region") \
+    .T \
+    .iloc[dskip:] \
+    .plot(figsize=(6,6))
+
+plt.xlabel("Date")
+plt.ylabel("Total COVID-19 Cases")
+plt.yscale("log")
+plt.minorticks_off()
+plt.gca().yaxis.set_minor_formatter(NullFormatter())
+plt.gca().yaxis.set_major_formatter(ScalarFormatter())
+
+timestamp = time.strftime("%Y%m%d")
+plt.savefig(f"{timestamp}.png")
