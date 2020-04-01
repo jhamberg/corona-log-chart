bacd87f2990699e5f46a669ac42da80bf6ab1bbe
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2020-04-01 16:28:26 +0300

    Add populations and support for command line arguments

--- plot.py
+++ plot.py
@@ -2,24 +2,32 @@
 
 import time
 import csv
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter, NullFormatter
 
-countries = [
-    'Finland',
-    'France',
-    'Germany',
-    'Iran',
-    'Italy',
-    'Korea, South',
-    'Spain',
-    'Sweden',
-    'US',
-    'United Kingdom'
-]
+population = {
+    'Finland': 5.513,
+    'Germany': 82.790,
+    'Iran': 81.160,
+    'Italy': 60.480,
+    'Japan': 126.800,
+    'Korea:  South': 51.470,
+    'Spain': 46.660,
+    'Sweden': 10.120,
+    'US': 327.000,
+    'United Kingdom': 66.440
+}
+countries = list(population.keys())
 
-url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+parser = argparse.ArgumentParser('plot.py')
+parser.add_argument('-d', '--data', dest='dataset', default='confirmed', help="dataset (default: confirmed), possible values: confirmed, recovered, deaths")
+parser.add_argument('-l', '--linear', dest='linear', action='store_true', help="use linear scale (default: false)")
+parser.add_argument('-pc', '--per-capita', dest='per_capita', action='store_true', help="show values per million people (default: false)")
+args = parser.parse_args()
+
+url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{args.dataset}_global.csv'
 dskip = 30 # Days to skip
 fig = pd.read_csv(url) \
     .drop(columns=["Lat", "Long"]) \
